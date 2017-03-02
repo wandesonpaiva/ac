93e102bec6a8c2fec85c04b6a6e35bc0254ba2a7
Wandeson Paiva <wandeson.jpaiva@gmail.com>
2017-03-02 08:38:26 -0300

    teste 'Aluno atingiu limite de aproveitamento ou certificação' e correções

--- test/models/RequerimentoTest.js
+++ test/models/RequerimentoTest.js
@@ -5,14 +5,18 @@ var Requerimento = require('../../app/models/Requerimento.js');
 var Aluno = require('../../app/models/Aluno.js');
 
 describe('# Teste da classe Requerimento', function() {
-  describe('## Construtores', function() {
-      it('Aluno não atingiu limite de aproveitamento ou certificação', function() {
-        var aluno = new Aluno("Wandeson Paiva", "20131014040340", 450);
-        var requerimento = new Requerimento(aluno,null,"aproveitamento");
+    it('Aluno não atingiu limite de aproveitamento ou certificação', function() {
+      var aluno = new Aluno("Wandeson Paiva", "20131014040340", 450);
+      var requerimento = new Requerimento(aluno,null,"aproveitamento");
 
-        requerimento.status.should.be.equal("confirmado");
-      });
+      requerimento.status.should.be.equal("confirmado");
+    });
 
+    it('Aluno atingiu limite de aproveitamento ou certificação', function() {
+      var aluno = new Aluno("Wandeson Paiva", "20131014040340", 1090);
+      var requerimento = new Requerimento(aluno,null,"aproveitamento");
 
-  });
+      requerimento.status.should.be.equal("negado");
+    });
+
 });
