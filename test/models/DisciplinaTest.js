1a9c1607f65cdd38c91aac4b4166645be1b76632
Wandeson Paiva <wandeson.jpaiva@gmail.com>
2017-03-03 10:35:35 -0300

    teste de disciplina

--- test/models/DisciplinaTest.js
+++ test/models/DisciplinaTest.js
@@ -7,7 +7,7 @@ var ComponenteCurricular = require('../../app/models/ComponenteCurricular.js');
 describe('# Teste da classe Disciplina', function() {
   describe('## Construtores', function() {
       it('Disciplina VALIDA', function() {
-        var componente_curricular = new ComponenteCurricular()
+        var componente_curricular = new ComponenteCurricular("Teste de software", 90)
         var disciplina = new Disciplina(componente_curricular, "cursando", "20171", [7,8]);
         disciplina.should.not.be.null;
       });
