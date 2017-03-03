3828251a176cf40260098a86ab7dee8199f9c58a
Wandeson Paiva <wandeson.jpaiva@gmail.com>
2017-03-03 07:27:19 -0300

    implementação disciplina

--- app/models/Disciplina.js
+++ app/models/Disciplina.js
@@ -1,8 +1,9 @@
-var Disciplina = function(componente_curricular, situacao, semestre_letivo){
+var Disciplina = function(componente_curricular, situacao, semestre_letivo, notas){
 
   this.componente_curricular = componente_curricular;
   this.situacao = situacao;
   this.semestre_letivo = semestre_letivo;
+  this.notas=notas;
 
 };
 
